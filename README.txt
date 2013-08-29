76f6884c3c932caab0ff13de3e5427088d5200aa
Xavi Torné <xavi@airpim.(none)>
2013-08-29 17:07:54 +0200

    First commit for first step tutorial

---
+++ README.txt
@@ -0,0 +1,14 @@
+pyrxa README
+==================
+
+Getting Started
+---------------
+
+- cd <directory containing this file>
+
+- $venv/bin/python setup.py develop
+
+- $venv/bin/initialize_pyrxa_db development.ini
+
+- $venv/bin/pserve development.ini
+
---
+++ pyrxa/models.py
@@ -0,0 +1,36 @@
+import datetime
+from sqlalchemy import (
+    Column,
+    Integer,
+    Text,
+    Unicode,
+    UnicodeText,
+    DateTime,
+    )
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy.orm import (
+    scoped_session,
+    sessionmaker,
+    )
+
+from zope.sqlalchemy import ZopeTransactionExtension
+
+DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode(255), unique=True, nullable=False)
+    password = Column(Unicode(255), nullable=False)
+    last_logged = Column(DateTime, default=datetime.datetime.utcnow)
+
+class Entry(Base):
+    __tablename__ = 'entries'
+    id = Column(Integer, primary_key=True)
+    title = Column(Unicode(255), unique=True, nullable=False)
+    body = Column(UnicodeText, default=u'')
+    created = Column(DateTime, default=datetime.datetime.utcnow)
+    edited = Column(DateTime, default=datetime.datetime.utcnow)
