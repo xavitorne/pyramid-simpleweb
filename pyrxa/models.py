9ee9982da2a5dfb8cc72f97d5bfe0adcdc47f507
Xavi Torné <xavitorne@gmail.com>
2013-09-02 20:39:50 +0200

    added new methods for class Entry

--- pyrxa/models.py
+++ pyrxa/models.py
@@ -1,4 +1,5 @@
 import datetime
+
 from sqlalchemy import (
     Column,
     Integer,
@@ -8,6 +9,7 @@ from sqlalchemy import (
     DateTime,
     )
 
+import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy.orm import (
@@ -20,6 +22,11 @@ from zope.sqlalchemy import ZopeTransactionExtension
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
+from webhelpers.text import urlify #<- will generate slugs
+from webhelpers.paginate import PageURL_WebOb, Page #<- provides pagination
+from webhelpers.date import time_ago_in_words #<- human friendly dates
+
+
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
@@ -27,10 +34,34 @@ class User(Base):
     password = Column(Unicode(255), nullable=False)
     last_logged = Column(DateTime, default=datetime.datetime.utcnow)
 
+
 class Entry(Base):
+
     __tablename__ = 'entries'
+
     id = Column(Integer, primary_key=True)
     title = Column(Unicode(255), unique=True, nullable=False)
     body = Column(UnicodeText, default=u'')
     created = Column(DateTime, default=datetime.datetime.utcnow)
     edited = Column(DateTime, default=datetime.datetime.utcnow)
+
+    @classmethod
+    def all(cls):
+        return DBSession.query(Entry).order_by(sa.desc(Entry.created))
+
+    @classmethod
+    def by_id(cls, id):
+        return DBSession.query(Entry).filter(Entry.id == id).first()
+
+    @property
+    def slug(self):
+        return urlify(self.title)
+
+    @property
+    def created_in_words(self):
+        return time_ago_in_words(self.created)
+
+    @classmethod
+    def get_paginator(cls, request, page=1):
+        page_url = PageURL_WebOb(request)
+        return Page(Entry.all(), page, url=page_url, items_per_page=5)
