adcade476278d27ac6d433787994720e1713aa4e
Xavi Torné <xavitorne@gmail.com>
2013-09-02 21:12:22 +0200

    added auth policy for blog_action

--- pyrxa/__init__.py
+++ pyrxa/__init__.py
@@ -1,5 +1,8 @@
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
+from pyramid.authentication import AuthTktAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
+from .security import EntryFactory
 
 from .models import (
     DBSession,
@@ -13,11 +16,18 @@ def main(global_config, **settings):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
-    config = Configurator(settings=settings)
+
+    authentication_policy = AuthTktAuthenticationPolicy('machupichubaobao')
+    authorization_policy = ACLAuthorizationPolicy()
+    config = Configurator(settings=settings,
+                          authentication_policy=authentication_policy,
+                          authorization_policy=authorization_policy
+        )
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('blog', '/blog/{id:\d+}/{slug}')
-    config.add_route('blog_action', '/blog/{action}')
+    config.add_route('blog_action', '/blog/{action}',
+                     factory='pyrxa.security.EntryFactory')
     config.add_route('auth', '/sign/{action}')
     config.scan()
     return config.make_wsgi_app()
