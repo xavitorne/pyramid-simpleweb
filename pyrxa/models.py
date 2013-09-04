39d81a14fe966032d45d6035e1febc761dc9672f
Xavi Torné <xavi@airpim.(none)>
2013-09-04 17:36:13 +0200

    added mainpage model

--- pyrxa/models.py
+++ pyrxa/models.py
@@ -72,3 +72,34 @@ class Entry(Base):
     def get_paginator(cls, request, page=1):
         page_url = PageURL_WebOb(request)
         return Page(Entry.all(), page, url=page_url, items_per_page=5)
+
+
+class MainPage(Base):
+    __tablename__ = 'pages'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(Unicode(255), unique=True, nullable=False)
+    body = Column(UnicodeText, default=u'')
+    created = Column(DateTime, default=datetime.datetime.utcnow)
+    edited = Column(DateTime, default=datetime.datetime.utcnow)
+
+    @classmethod
+    def all(cls):
+        return DBSession.query(Entry).order_by(sa.desc(Entry.created))
+
+    @classmethod
+    def by_id(cls, id):
+        return DBSession.query(Entry).filter(Entry.id == id).first()
+
+    @property
+    def slug(self):
+        return urlify(self.title)
+
+    @property
+    def created_in_words(self):
+        return time_ago_in_words(self.created)
+
+    @classmethod
+    def get_paginator(cls, request, page=1):
+        page_url = PageURL_WebOb(request)
+        return Page(Entry.all(), page, url=page_url, items_per_page=5)
